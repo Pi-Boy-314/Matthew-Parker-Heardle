234481683a9b3d05abc2092fa8d692f4dbf65b87
Pi-Boy-314 <raspberry.pi.3.14.luke@gmail.com>
2026-01-26 13:55:52 -0600

    More improvements to search normalization

--- src/components/GuessBar.vue
+++ src/components/GuessBar.vue
@@ -18,6 +18,10 @@ function normalizeText(text: string): string {
     .replace(/[Üü]/g, 'U')
     .replace(/[Öö]/g, 'O')
     .replace(/[Ää]/g, 'A')
+    .replace(/['']/g, "'") // Normalize smart quotes/apostrophes
+    .replace(/[♥‿]/g, '') // Remove heart/face emoticons
+    .replace(/[><_]/g, '') // Remove emoticon symbols
+    .replace(/[~]/g, '') // Remove tildes
     .toLowerCase();
 }
 
